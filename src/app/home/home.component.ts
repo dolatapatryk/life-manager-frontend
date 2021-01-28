4066e8a10e2a2fdc74b6694fd5f303a92d61e998
dolatapatryk1 <dolata.patryk1@gmail.com>
2021-01-28 19:05:24 +0100

    created account module

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+import {User} from '../models/user';
+
+@Component({
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss']
+})
+export class HomeComponent {
+    user: User;
+
+    constructor() {
+    }
+}
